2626b77c79a7cd18f7f7e3347c47a1feb8164652
Ian Boyle <irboyle21@gmail.com>
2022-01-21 10:30:06 -0500

    adds InvoiceList and InvoiceList interface

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,11 +3,31 @@ import * as React from "react";
 import Hello from "./Hello";
 import HelloWithHooks from "./HelloWithHooks";
 
+interface IInvoiceListProps {
+  customerName: string,
+  invoices: {id: number; name: string; total: string}
+}
+
+const InvoiceList = (props: IInvoiceListProps) => {
+  return <div>
+    <h1>Invoices...</h1>
+    <h2>{props.invoices}</h2>
+  </div>
+}
+
 const App = () => {
+  const data = {
+    customerName: "Google",
+    invoices: [
+      {id: 123, name: "Dev Work", total: "20.00"},
+      {id: 456, name: "Dev Work", total: "50.00"},
+      {id: 789, name: "Other Work", total: "100.00"}
+    ]
+  }
+
   return (
     <div>
-      <Hello compiler={"TS"} framework={"React"} />
-      <HelloWithHooks />
+      <InvoiceList invoiceData={data}/>
     </div>
   );
 };
