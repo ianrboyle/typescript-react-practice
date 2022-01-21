427df95a92d6e4005b7965dd9f4664fdc75f630c
Ian Boyle <irboyle21@gmail.com>
2022-01-21 13:57:54 -0500

    adds invoicedata type and invoice type

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,29 +4,44 @@ import Hello from "./Hello";
 import HelloWithHooks from "./HelloWithHooks";
 
 interface IInvoiceListProps {
-  invoiceData: {
-    customerName: string,
-    invoices: {id: number; name: string; total: string}[]
-  }
+  invoiceData: InvoiceDataType;
   logo?: string
 
 }
 
+type InvoiceType =  {id: number; name: string; total: string; paymentStatus: string}
+
+type InvoiceDataType = {
+  customerName: string;
+  invoices: InvoiceType[]
+}
 const InvoiceList = (props: IInvoiceListProps) => {
   const {customerName, invoices} = props.invoiceData
+  const invoicesInfo = invoices.map((invoice) => {return invoice})
+
+  //diffing engine
   return <div>
     <h1>Invoices...</h1>
     <h2>{customerName}</h2>
+    
+    {invoices.map((invoice) => (
+    <div 
+    key={invoice.id}
+    style={{display: "flex", justifyContent: "space-between"}}>
+    <div>{invoice.name}</div>
+    <div>{invoice.total}</div>
+    <div>{invoice.paymentStatus}</div>
+    </div>))}
   </div>
 }
 
 const App = () => {
-  const data = {
+  const data: InvoiceDataType = {
     customerName: "Google",
     invoices: [
-      {id: 123, name: "Dev Work", total: "20.00"},
-      {id: 456, name: "Dev Work", total: "50.00"},
-      {id: 789, name: "Other Work", total: "100.00"}
+      {id: 123, name: "Dev Work", total: "20.00", paymentStatus: "paid"},
+      {id: 456, name: "Dev Work", total: "50.00", paymentStatus: "paid"},
+      {id: 789, name: "Other Work", total: "100.00", paymentStatus: "not paid"}
     ]
   }
 
